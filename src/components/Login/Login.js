82054c748de299283cdc5679a10fcd4736378233
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-10-26 04:41:25 +0600

    Login-Form-completed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
     return (
         <div>
-             <Form>
+             <Form className="w-50 loginForm">
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+       
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+     <div>
+     <Link to='/register'><span>Register now </span></Link>
+     </div>
       <Button variant="primary" type="submit">
-        Submit
+        Login
       </Button>
     </Form>
         </div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 const Register = () => {
     return (
         <div>
-            <h2>Register page</h2>
+            <Form className="w-50 loginForm">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="name" name="name" placeholder="Full Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Photo URL </Form.Label>
+        <Form.Control type="photoURL" name="photo" placeholder="Photo URL" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" name="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" name="password" placeholder="Password" />
+      </Form.Group>
+     <div>
+     <Link to='/register'><span>Register now </span></Link>
+     </div>
+      <Button variant="info" type="submit">
+        Register 
+      </Button>
+    </Form>
         </div>
     );
 };

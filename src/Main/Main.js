403781118ae1d09f375ac2d5c5c7305576dd8524
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-10-27 17:53:00 +0600

    added-footer

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -21,6 +21,7 @@ const Main = () => {
 
                 </Row>
             </Container>
+            
         </div>
     );
 };
--- src/pages/share/Footer/Footer.js
+++ src/pages/share/Footer/Footer.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>this is footer</h2>
+  return (
+    <div>
+      <h2>this is footer</h2>
+      <div class="card text-center">
+        <div class="card-header">Featured</div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
         </div>
-    );
+        <div class="card-footer text-muted">2 days ago</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
